104c9f466a012970d03c382233d4403751e99130
Agus benavent <you@example.com>
2023-11-09 18:00:57 -0300

    ja

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -1,32 +1,56 @@
 
-let evento4= document.querySelector(".scrolly")
-    evento4.addEventListener("mouseover", function(){
-    document.querySelector(".evento4").style.color = colorAlAzar()
-    })
+// let evento4= document.querySelector(".scrolly")
+//     evento4.addEventListener("mouseover", function(){
+//     document.querySelector(".evento4").style.color = colorAlAzar()
+//     })
 
-let evento5= document.querySelector(".scrolly")
-evento4.addEventListener("mouseout", function(){
-    document.querySelector(".evento4").style.color = "#888"
-    })
+// let evento5= document.querySelector(".scrolly")
+// evento4.addEventListener("mouseout", function(){
+//     document.querySelector(".evento4").style.color = "#888"
+//     })
 
-//Ejercicio 9
+// //Ejercicio 9
 
-let evento6 = document.querySelector("#lechuza")
-evento6.addEventListener("click", function(){
-alert("Buh… Buh…")
-})
+// let evento6 = document.querySelector("#lechuza")
+// evento6.addEventListener("click", function(){
+// alert("Buh… Buh…")
+// })
 
-//Ejercicio 10
+// //Ejercicio 10
+
+// window.addEventListener("keypress",function(e){
+//     console.log("Ey, tocaste el teclado!")
+// })
+
+// //Ejercicio 11
+
+// window.addEventListener("keypress",function(e){
+//     console.log(e)
+//     if (e.key === " ") {
+//         console.log("Ey, tocaste la barra!");
+//       }
+// })
+
+let boton = document.querySelector(".button")
+boton.addEventListener("click",function(){
+    alert("peli")
+});
 
 window.addEventListener("keypress",function(e){
-    console.log("Ey, tocaste el teclado!")
+    
+    if(e.key === "Enter"){
+        this.alert("Ey")
+    }
 })
 
-//Ejercicio 11
 
-window.addEventListener("keypress",function(e){
-    console.log(e)
-    if (e.key === " ") {
-        console.log("Ey, tocaste la barra!");
-      }
+let apiKey = "175e62bf80c432367c7a248221db5359"
+
+let cargarpeliculas = {
+    fetch('')//Nos devuleve una promesa 
+}
+
+
+.then(function(data){
+    for(let i= 0; i < data)
 })
