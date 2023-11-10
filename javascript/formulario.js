bbd081033a85279efcc54b2a5c08e37412a56a98
Agus benavent <you@example.com>
2023-11-10 08:50:44 -0300

    6

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -1,35 +1,7 @@
 
-// let evento4= document.querySelector(".scrolly")
-//     evento4.addEventListener("mouseover", function(){
-//     document.querySelector(".evento4").style.color = colorAlAzar()
-//     })
 
-// let evento5= document.querySelector(".scrolly")
-// evento4.addEventListener("mouseout", function(){
-//     document.querySelector(".evento4").style.color = "#888"
-//     })
 
-// //Ejercicio 9
-
-// let evento6 = document.querySelector("#lechuza")
-// evento6.addEventListener("click", function(){
-// alert("Buh… Buh…")
-// })
-
-// //Ejercicio 10
-
-// window.addEventListener("keypress",function(e){
-//     console.log("Ey, tocaste el teclado!")
-// })
-
-// //Ejercicio 11
-
-// window.addEventListener("keypress",function(e){
-//     console.log(e)
-//     if (e.key === " ") {
-//         console.log("Ey, tocaste la barra!");
-//       }
-// })
+//PUNTO 1
 
 let boton = document.querySelector(".button")
 boton.addEventListener("click",function(){
@@ -43,29 +15,19 @@ window.addEventListener("keypress",function(e){
     }
 })
 
-<form action="resultados.html" method="GET">
-    <input type="text" name="buscador" value="">
-    <button type="submit">Enviar</button>
-</form>
 
-let queryString = location.search;
-let queryStringObj = new URLSearchParams (queryString);
-queryStringObj.get ('buscador'); // alien
+//PUNTO 1.3
+
 
 
 
 
 // let apiKey = "175e62bf80c432367c7a248221db5359"
 
-// let cargarpeliculas = {
-//     fetch('')//Nos devuleve una promesa 
-// }
 
 
 // .then(function(data){
 //     for(let i= 0; i < data)
 // })
 
-
-//QUERYSTRING
 
