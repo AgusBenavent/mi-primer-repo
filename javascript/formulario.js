aa99e5a022044adea3921e64f4a0e6c2f60a6551
Agus benavent <you@example.com>
2023-11-10 10:04:11 -0300

    7

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -16,13 +16,10 @@ window.addEventListener("keypress",function(e){
 })
 
 
-//PUNTO 1.3
 
 
 
-
-
-// let apiKey = "175e62bf80c432367c7a248221db5359"
+// let apiKey = ""
 
 
 
@@ -31,3 +28,22 @@ window.addEventListener("keypress",function(e){
 // })
 
 
+
+
+fetch("https://api.themoviedb.org/3/discover/movie?api_key=175e62bf80c432367c7a248221db5359")//Siempre va api_key y si quiero mas parametros pongo &
+.then(function(response){
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+
+    // <article class="peli1">
+    //             <a href="./detailmovie.html"><img src="./img/peliculas_populares/alien.png" class="img"></a>
+    //         <div class="parrafo"><h3>Titulo: Alien</h3>
+    //             <p>Fecha de estreno: 25 de diciembre de 1979</p>
+    //         </div>
+    //         </article>
+})
+.catch(function(error){
+    console.log("El error es: "+ error)
+})
