3e30109b52642e240ca51ef2c3e251f46bd60ad3
Cata Rodriguez <cata@MacBook-Air-de-Cata.local>
2023-11-17 08:19:34 -0300

    Buscador

--- javascript/search-results.js
+++ javascript/search-results.js
@@ -1,10 +1,20 @@
 //Punto 2 - 
+
+// Se declara la variable
 let Buscador = ""
+
+let resultadoTexto = document.querySelector(".resultado-texto");
+
+// Se obtiene la cadena de consulta de la ubicación actual (URL)
 let queryString = location.search;
-let queryStringObj = new URLSearchParams (queryString); //lectura d eparametros
-Buscador = queryStringObj.get ('Buscador');  
+
+// Se crea un nuevo objeto URLSearchParams para leer los parámetros
+let queryStringObj = new URLSearchParams (queryString);
+
+Buscador = queryStringObj.get ('Buscador'); 
 
 let buscador1 = document.querySelector(".busqueda")
+
 
 fetch(`https://api.themoviedb.org/3/search/multi?api_key=175e62bf80c432367c7a248221db5359&query=${Buscador}`)
 .then(function(response){
@@ -12,17 +22,20 @@ fetch(`https://api.themoviedb.org/3/search/multi?api_key=175e62bf80c432367c7a248
 })
 .then(function(data){
     console.log(data);
-    for(let i=0;i<5 ; i++){
-        buscador1.innerHTML+=
-        `
-        <a href= "../detailmovie.html">
-        <div class = "busqueda">
-        <img src="https://image.tmdb.org/t/p/w500${data.results[i]}">
-        <a href= 
-        </div>`
-    }
+    if (data.results.length === 0) {
+        buscador1.innerHTML = "No hay resultados para su búsqueda.";
+    } else {  
+        resultadoTexto.textContent = `Resultados de búsqueda para: ${Buscador}`;
+        for(let i=0;i<5 ; i++){
+            buscador1.innerHTML+= `
+            <a href="./detailmovie.html?id=${data.results[i].id}"> 
+            <img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}"class="img">
+            </a>
+    `;} 
+    
+}})             // CONECTAR A CADA DETALLE
+    
 
-})
 .catch(function(error){
     console.log("El error es: "+ error)
 })
