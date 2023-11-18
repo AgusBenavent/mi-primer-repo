29fc8fbb700daf3b38478d9f89f5e1d801af2d9d
Cata Rodriguez <cata@Air-de-Cata.fibertel.com.ar>
2023-11-17 22:42:15 -0300

    ddd

--- javascript/detail.serie.js
+++ javascript/detail.serie.js
@@ -69,8 +69,8 @@ let recomendaciones2 = document.querySelector(".reco")
 
             <a href="./detailmovie.html?id=${data.results[i].id}">
             <img src="https://image.tmdb.org/t/p/w500${data.results[i].poster_path}" class="img"></a>
-            <div class="parrafo"><h3> Titulo: ${data.results[i].original_title}</h3>
-            <p>Fecha de estreno: ${data.results[i].release_date}</p>
+            <div class="parrafo"><h3> Titulo: ${data.results[i].original_name}</h3>
+            <p>Fecha de estreno: ${data.results[i].first_air_date}</p>
             </div>
         
     </section>`
